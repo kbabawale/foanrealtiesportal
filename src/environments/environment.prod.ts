078a579e7e4f8fdda6e6bb4b1ebaa110dacad512
Kolapo Babawale <kolapobabawale@gmail.com>
2019-06-29 12:24:02 +0100

    change to environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://179.78.130.102:9000',
-  apiUrl2: 'http://179.78.130.102/foan_realities/images/',
-  apiUrl3: 'http://179.78.130.102/foan_realities/'
+  apiUrl: 'http://178.79.130.102:9000',
+  apiUrl2: 'http://178.79.130.102/foan_realities/images/',
+  apiUrl3: 'http://178.79.130.102/foan_realities/'
 };
